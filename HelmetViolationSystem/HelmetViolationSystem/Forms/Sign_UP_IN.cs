0463f18707d451244599aa73345c25326d8244b7
Dunghoang206 <thanhcat123456dm@gmail.com>
2026-09-24 00:37:29 +0700

    Create Form Sign In/Up

---
+++ HelmetViolationSystem/HelmetViolationSystem/Forms/Sign_UP_IN.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelmetViolationSystem.Forms
+{
+    public partial class Sign_UP_IN : Form
+    {
+        public Sign_UP_IN()
+        {
+            InitializeComponent();
+        }
+    }
+}
